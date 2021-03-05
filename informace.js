444188118d0da19a51a786acec154c3d74259201
Ota-Prokopec <70975493+Ota-Prokopec@users.noreply.github.com>
2021-03-05 19:35:26 +0100

    Add files via upload

--- informace.js
+++ informace.js
@@ -7,31 +7,56 @@ const informace = {
 
 
 };
-
-let idpos = 0;
-let value = "ahoj";
-
-
-
-window.addEventListener("click", clicked);
-
-
-
-
-function clicked() {
-  value = textinput.value;
-if (event.srcElement.className === "pole") {
-      if (event.srcElement.id !== "") {
-        const data = event.srcElement.id;
-            console.log(informace[data]);
-      }
-      else {
-        idpos ++;
-         event.srcElement.id = "a" + idpos;
-         informace["a" + idpos] = value;
-         textinput.value = "";
-      }
-
-
+/*
+const informace = {
+  a1:
+  {
+  year:2021,
+  month:4,
+  day:4
+  text:"ahoj"
 }
+}
+*/
+let idpos = 0;
+let value = "";
+
+
+
+window.addEventListener("click", clicks);
+
+
+
+
+function clicks() {
+      let muzuNapsat = true;
+    if (event.srcElement.className === "pole") {
+      value = textinput.value;
+      for (const [key, value] of Object.entries(informace)) {
+      const pos = event.srcElement.getAttribute("pos");
+        if ("a" + parseInt(iMonth.value)+"a" + parseInt(pos) + "a" + parseInt(iYear.value) === key) {
+          if (value.year === iYear.value && value.month === parseInt(iMonth.value)) {
+            muzuNapsat = false;
+            console.log(informace);
+            console.log("precist zpravu ju", value.text);
+          }
+
+        }
+      }
+      if (muzuNapsat) {
+        const day = event.srcElement.getAttribute("pos");
+        const text = value;
+            const element = document.getElementsByClassName("pole") ;
+            for (let i= 0; i< element.length; i ++) {
+              if (element[i] === event.srcElement) {
+                idpos = i+1;
+              }
+            }
+
+          add(day, text);
+      }
+    }
+}
+function add(day, text) {
+      informace["a" + parseInt(iMonth.value)+"a"+idpos + "a" + parseInt(iYear.value)] = {month:parseInt(iMonth.value), year:iYear.value, day:day, text:text};
 }
--- js.js
+++ js.js
@@ -4,17 +4,31 @@ const pocetDnuVMesici = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const date = new Date();
 const array = document.getElementById("center");
 
-
+document.getElementById("month").value = new Date().getMonth() + 1;
 
 function doCalendar(day, month) {
 
          remove();
 
-    for (let i = 0; i < month; i ++) {
+    for (let i = 1; i < month+1; i ++) {
       if (day > 6) {
         day = 0;
       }
-      const element = Ota.createElementByTagName("div", {class:"pole"}, `${days[day]}    ${i + 1}`);
+      let dataInformace = "";
+      dataInformace = false;
+      for (const [key, value] of Object.entries(informace)) {
+        if (key === "a" + parseInt(iMonth.value) + "a" + i + "a" + parseInt(iYear.value)) {
+             dataInformace = value.text;
+        }
+      }
+      let element;
+      if (dataInformace !== false) {
+         element = Ota.createElementByTagName("div", {class:"pole"}, `${days[day]}    ${i}    ${dataInformace}`);
+      }
+      else {
+         element = Ota.createElementByTagName("div", {class:"pole"}, `${days[day]}    ${i }`);
+      }
+      element.setAttribute("pos", i);
       appendElement(array, element);
       day ++;
     }
